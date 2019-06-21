00d87c5d155b325a0b9924233e6409424fb56146
kguttas <kguttas@hotmail.com>
2019-06-21 09:25:58 -0400

    Se crea proyecto

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React  from 'react';
+
+class App extends React.Component {
+	
+
+	constructor(props, context) {
+		super(props, context);
+
+	}
+
+	render(){
+		return (
+		<div>HOLA</div>
+	 );
+				}
+}
+
+export default App;
